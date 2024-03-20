ed66b6204e2f9bf241f9c4911eb5ecde8ac8dfed
ductxph29059 <137851512+ductxph29059-herry@users.noreply.github.com>
2024-03-20 09:00:11 +0700

    Crud - đánh giá

--- AppSeller/App.tsx
+++ AppSeller/App.tsx
@@ -24,6 +24,7 @@ import HeaderRightComponent from './src/component/options-menu/HeaderRightCompon
 import {Provider} from 'react-redux';
 import store from './src/redux/store';
 import EditMonScreen from './src/screen/mon/EditMonScreen';
+import EvaluateScreen from './src/screen/danhGia/EvaluateScreen';
 
 const Stack = createStackNavigator();
 interface AppProps {}
@@ -114,6 +115,12 @@ const App: React.FC<AppProps> = () => {
                 name="DetailHoaDonScreen"
                 component={DetailHoaDonScreen}
                 options={{title: 'Chi tiết hóa đơn'}}
+              />
+                {/* Trang quản lý đánh giá */}
+                <Stack.Screen
+                name="EvaluateScreen"
+                component={EvaluateScreen}
+                options={{title: 'Đánh giá '}}
               />
             </Stack.Navigator>
           </NavigationContainer>
