83d001dc42b55f29e16ce98104c4e6ce438dd54f
huyhoangvn <47599913+huyhoangvn@users.noreply.github.com>
2024-03-26 11:28:47 +0700

    fix:nav-props

--- AppSeller/src/models/props/NavProps.ts
+++ AppSeller/src/models/props/NavProps.ts
@@ -5,6 +5,7 @@ interface NavProps {
       routes: { name: string; params?: { [key: string]: string } }[];
     }) => void;
     setOptions: (options: object) => void;
+    navigate: (name: string, params?: object) => void; // Add navigate property
   };
 }
 
