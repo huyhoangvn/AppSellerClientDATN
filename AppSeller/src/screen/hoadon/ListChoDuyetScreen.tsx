c3793c23aebed942f8063e9953782eb39c11191a
vanquyet16 <a ch email ca bn trn GitHub>
2024-04-07 13:20:18 +0700

    hoá đơn fixxx add ngày

--- AppSeller/src/screen/hoadon/ListChoDuyetScreen.tsx
+++ AppSeller/src/screen/hoadon/ListChoDuyetScreen.tsx
@@ -1,5 +1,15 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, StyleSheet, FlatList, TouchableOpacity, Image, KeyboardAvoidingView, ScrollView, Platform} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from 'react-native';
 import NavProps from '../../models/props/NavProps';
 import EditTextComponent from '../../component/EditTextComponent';
 import {
@@ -8,17 +18,21 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import {appColors} from '../../constants/appColors';
 import DropDownComponent from '../../component/DropDownComponent';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
-import { text } from '@fortawesome/fontawesome-svg-core';
-import { NhanVien } from '../../models/NhanVien';
-import { HoaDon } from '../../models/HoaDon';
+import {text} from '@fortawesome/fontawesome-svg-core';
+import {HoaDon} from '../../models/HoaDon';
 import authenticationAPI from '../../apis/authApi';
 import AlertComponent from '../../component/AlertComponent';
 import LoadingComponent from '../../component/LoadingComponent';
+import {getData} from '../../utils/storageUtils';
+import EditText from '../../component/edittext/EditText';
+import {appFontSize} from '../../constants/appFontSizes';
 const ListChoDuyetScreen: React.FC<NavProps> = ({navigation}) => {
+  const [selectedItem, setSelectedItem] = useState<any>(null);
   const [text, setText] = useState('Xem thêm');
   const [data, setData] = useState<HoaDon[]>([]);
   const [dataNew, setDataNew] = useState<HoaDon[]>([]);
@@ -26,25 +40,29 @@ const ListChoDuyetScreen: React.FC<NavProps> = ({navigation}) => {
   const [showAlert, setShowAlert] = useState(false);
   const [msg, setMsg] = useState('');
   const [page, setPage] = useState(1);
-  const [purchase, setPurchase] = useState(0);
+  const [purchase, setPurchase] = useState('');
+  const [payment, setPayment] = useState('');
   const [code, setCode] = useState('');
+  const [date, setDate] = useState<any>();
+  const [selectedDate, setSelectedDate] = useState<Date>();
+  const [position, setPosition] = useState<any>();
 
- const getStatusText = (status: number): string => {
-  switch (status) {
-    case 0:
-      return 'Chờ duyệt';
-    case 1:
-      return 'Đang chuẩn bị';
-    case 2:
-      return 'Đang giao hàng';
-    case 3:
-      return 'Giao hàng thành công';
-    case 4:
-      return 'Giao hàng thất bại';
-    default:
-      return 'Trạng thái không xác định';
-  }
-};
+  const getStatusText = (status: number): string => {
+    switch (status) {
+      case 0:
+        return 'Chờ duyệt';
+      case 1:
+        return 'Đang chuẩn bị';
+      case 2:
+        return 'Đang giao hàng';
+      case 3:
+        return 'Giao hàng thành công';
+      case 4:
+        return 'Giao hàng thất bại';
+      default:
+        return 'Trạng thái không xác định';
+    }
+  };
   const handleShowAlert = () => {
     setShowAlert(true);
   };
@@ -54,10 +72,8 @@ const ListChoDuyetScreen: React.FC<NavProps> = ({navigation}) => {
   };
 
   const actionSearch = async (item: string) => {
-    getListInvoice(item,purchase,page);
-
+    await getListInvoice(item, 0, page);
   };
-
 
   const handelDetail = (item: any) => {
     navigation.navigate('DetailHoaDonScreen', {
@@ -65,94 +81,105 @@ const ListChoDuyetScreen: React.FC<NavProps> = ({navigation}) => {
     });
   };
 
-
+  const handleGetAll = async () => {
+    await getListInvoice(code, 0, page + 1);
+  };
 
   const getListInvoice = async (
     code?: any,
     purchaseStatus?: any,
     page?: any,
-
-
   ) => {
     try {
-      const res = await authenticationAPI.HandleAuthentication(
+      setLoading(true); // Set loading to true before making the API call
+      const res: any = await authenticationAPI.HandleAuthentication(
         `/nhanvien/hoaDon?maHD=${code}&trangThaiMua=${purchaseStatus}&trang=${page}`,
         'get',
       );
-        
-      if (res.success === true) {
-        if (res.list.length !== 0 && res.currentPage === 1) {
-          setData(res.list);
-          setDataNew([]);
-        } else if (res.list.length !== 0 && res.currentPage !== 1) {
-          setData(prevData => [...prevData, ...res.list]);
-        } else {
-          setData([]);
-          // setText('Hết dữ liệu');
-          // setMsg('Đã đến cuói danh sách');
-          // handleShowAlert();
+
+      if (res.success === false) {
+        if (!res.list) {
+          return;
         }
+        return;
+      }
+
+      if (page === 1) {
+        setData([...res.list]);
       } else {
-        // Xử lý khi có lỗi từ API
-        setMsg('Request failed. Please try again.');
-        handleShowAlert();
+        setData(prevData => [...prevData, ...res.list]);
+      }
+      if (res.list.length > 0) {
+        setPage(page);
+        setText(res.list.length === 10 ? 'Xem Thêm' : 'Hết');
+      } else {
+        setText('Hết');
       }
       setCode(code);
       setPurchase(purchaseStatus);
-      setPage(page);
-   
-    } catch (err) {
-      console.log(err);
-      setMsg('Request timeout. Please try again later.');
-      handleShowAlert();
+      setDate(date);
+    } catch (error) {
+      console.error(error);
     } finally {
       setLoading(false);
     }
   };
 
-  const handleGetAll = async () => {
-    try {
-      setLoading(true);
-      const nextPage = page + 1; // Tăng giá trị của currentPage lên 1
-      await getListInvoice(code,0,nextPage);
-    } catch (error) {
-      console.error('Error loading next page:', error);
-    } finally {
-      setLoading(false);
-    }
+  const formatDate = (dateTimeString: any) => {
+    const dateTime = new Date(dateTimeString);
+
+    const day = dateTime.getDate();
+    const month = dateTime.getMonth() + 1; // JavaScript month is zero-based
+    const year = dateTime.getFullYear();
+
+    const formattedDate = `${day < 10 ? '0' : ''}${day}/${
+      month < 10 ? '0' : ''
+    }${month}/${year}`;
+
+    const hours = dateTime.getHours();
+    const minutes = dateTime.getMinutes();
+    const formattedTime = `${hours < 10 ? '0' : ''}${hours}:${
+      minutes < 10 ? '0' : ''
+    }${minutes}`;
+
+    return {formattedDate, formattedTime};
   };
+
   useEffect(() => {
-    getListInvoice('',0,page);
+    getListInvoice('', 0, page);
   }, []);
 
-
-
-
- 
   const renderItem = ({item}: {item: HoaDon}) => {
+    const {formattedDate, formattedTime} = formatDate(item.thoiGianTao);
     return (
       <TouchableOpacity onPress={() => handelDetail(item)}>
         <View style={styles.item}>
-        
           <View style={{paddingHorizontal: 10}}>
             <Text style={{fontWeight: 'bold', color: 'black'}}>
               MHD:{item.maHD}
             </Text>
             <Text style={{fontWeight: 'bold', color: 'black'}}>
-              Tổng tiền: {parseInt(item.tongTien || '').toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })}
+              Tổng tiền:{' '}
+              {parseInt(item.tongTien || '').toLocaleString('vi-VN', {
+                style: 'currency',
+                currency: 'VND',
+              })}
             </Text>
             <Text style={{color: 'black', fontWeight: 'bold'}}>
               {/* {item.trangThaiMua === 1 ? "ok" : "Chưa mua"} */}
               Trạng thái mua: {getStatusText(item.trangThaiMua ?? 0)}
             </Text>
-            <Text style = {{fontWeight: 'bold', color: 'black'}}>
-              Thanh toán: 
-            {item.trangThaiThanhToan === 0 ? (
-              <Text style={{ color: 'red' }}> Chưa thanh toán</Text>
-            ) : (
-              <Text style={{ color: 'green' }}> Đã thanh toán</Text>
-            )}
-          </Text>
+            <Text style={{fontWeight: 'bold', color: 'black'}}>
+              Thanh toán:
+              {item.trangThaiThanhToan === 0 ? (
+                <Text style={{color: 'red'}}> Chưa thanh toán</Text>
+              ) : (
+                <Text style={{color: 'green'}}> Đã thanh toán</Text>
+              )}
+            </Text>
+            <Text style={{color: 'black', fontWeight: 'bold'}}>
+              Ngày tạo: {formattedDate || ''} - {formattedTime || ''}
+            </Text>
           </View>
         </View>
       </TouchableOpacity>
@@ -161,84 +188,82 @@ const ListChoDuyetScreen: React.FC<NavProps> = ({navigation}) => {
 
   return (
     <KeyboardAvoidingView
-    style={{ flex: 1 }}
-    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-    keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : 20} // Điều chỉnh vị trí của bàn phím
-  >
-    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
- 
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <EditTextComponent
-          label="iconRight"
-          placeholder="Nhập mã hoá đơn"
-          iconRight={faMagnifyingGlass}
-          stylesEdit={{backgroundColor: 'white'}}
-          onChangeText={(text: string) => actionSearch(text)}
-          stylesContainer={{
-            backgroundColor: appColors.white,
-            borderColor: 'black',
-            borderWidth: 1.5,
-          }}
-          iconColor={appColors.primary}
-        />
-
-      </View>
-      <View style={styles.main}>
-        {data.length === 0 ? (
-          <View>
-            <Text style={{textAlign: 'center', fontSize: 20}}>
-              không tìm thấy nhân viên
-            </Text>
-            <TouchableOpacity 
-            onPress={() => {getListInvoice('',0,1) , setPage(1)}}
-            >
-              <Text
-                style={{
-                  textAlign: 'center',
-                  marginTop: 20,
-                  color: appColors.primary,
-                  textDecorationLine: 'underline',
-                }}>
-                Trở lại
-              </Text>
-            </TouchableOpacity>
-          </View>
-        ) : (
-          <FlatList
-            data={data}
-            renderItem={renderItem}
-            keyExtractor={item => item._id || ''}
-            // onScroll={() => { setScroll(true), setLastList(false) }} // Khi cuộn, đánh dấu là đã cuộn
-            // onEndReached={() => { setLastList(true), setScroll(false) }} // Kích hoạt khi đạt đến cuối danh sách
-            // onEndReachedThreshold={.1}
-            scrollEnabled={false}
-            ListFooterComponent={() => (
-              <View style={{alignItems: 'center', paddingVertical: 10}}>
-                {/* {lastList === true && scroll !== true ? ( */}
-                <TouchableOpacity 
-                onPress={handleGetAll}
-                  >
-                  <Text style={{fontSize: 14}}>{text}</Text>
-                </TouchableOpacity>
-                {/* ) : null} */}
-              </View>
-            )}
+      style={{flex: 1, backgroundColor: 'white'}}
+      contentContainerStyle={{flexGrow: 1}}
+      // Tùy chỉnh khoảng cách cuộn thêm khi bàn phím hiển thị
+    >
+      <View style={styles.container}>
+        {/* <ScrollView> */}
+        <View style={styles.header}>
+          <EditTextComponent
+            label="iconRight"
+            placeholder="Nhập mã hoá đơn"
+            iconRight={faMagnifyingGlass}
+            stylesEdit={{backgroundColor: 'white'}}
+            onChangeText={(text: string) => actionSearch(text)}
+            stylesContainer={{
+              backgroundColor: appColors.white,
+              borderColor: 'black',
+              borderWidth: 1.5,
+              elevation: 1,
+            }}
+            iconColor={appColors.primary}
           />
-        )}
-      </View>
-      <View>
-      <LoadingComponent visible={loading} />
-      <AlertComponent
-        visible={showAlert}
-        message={msg}
-        onClose={handleCloseAlert}
-      />
-      </View>
-    </View>
-    </ScrollView>
-     </KeyboardAvoidingView>
+        </View>
 
+        <View style={styles.main}>
+          {data.length === 0 ? (
+            <View style={{height: hp(100)}}>
+              <Text style={{textAlign: 'center', fontSize: 20}}>
+                không tìm thấy nhân viên
+              </Text>
+              <TouchableOpacity
+                onPress={async () => {
+                  await getListInvoice('', 0, 1), setPage(1);
+                }}>
+                <Text
+                  style={{
+                    textAlign: 'center',
+                    marginTop: 20,
+                    color: appColors.primary,
+                    textDecorationLine: 'underline',
+                  }}>
+                  Trở lại
+                </Text>
+              </TouchableOpacity>
+            </View>
+          ) : (
+            <FlatList
+              data={data}
+              renderItem={renderItem}
+              keyExtractor={item => item._id || ''}
+              scrollEnabled={true}
+              style={{height: hp(80)}}
+              // onScroll={() => { setScroll(true), setLastList(false) }} // Khi cuộn, đánh dấu là đã cuộn
+              // onEndReached={() => { setLastList(true), setScroll(false) }} // Kích hoạt khi đạt đến cuối danh sách
+              // onEndReachedThreshold={.1}
+              ListFooterComponent={() => (
+                <View style={{alignItems: 'center', paddingVertical: 10}}>
+                  <TouchableOpacity onPress={handleGetAll}>
+                    <Text style={{fontSize: appFontSize.normal}}>{text}</Text>
+                  </TouchableOpacity>
+                </View>
+              )}
+            />
+          )}
+        </View>
+
+        <View>
+          <LoadingComponent visible={loading} />
+          <AlertComponent
+            visible={showAlert}
+            message={msg}
+            onClose={handleCloseAlert}
+          />
+        </View>
+        {/* </ScrollView> */}
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -247,18 +272,23 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   header: {
-    flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+  },
+  viewDropDow: {
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   main: {
-    flex: 7,
+    flex: 2,
   },
+
   item: {
     marginHorizontal: 10,
     padding: 10,
     borderColor: 'black',
     borderWidth: 0.5,
-    marginBottom: 15,
+    marginTop: 15,
     borderRadius: 10,
     flexDirection: 'row',
   },
