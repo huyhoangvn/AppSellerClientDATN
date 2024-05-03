73d6672f15736f900e921e0997a9307b9da4d772
huyhoangvn <47599913+huyhoangvn@users.noreply.github.com>
2024-05-03 08:40:55 +0700

    Update appInfos.ts

--- AppSeller/src/constants/appInfos.ts
+++ AppSeller/src/constants/appInfos.ts
@@ -1,5 +1,12 @@
 import {Dimensions} from 'react-native';
 
 export const appInfo = {
+  //Máy Quyết
+  // BASE_URL: 'http://192.168.43.127:3000',
+
+  //Máy Vũ
+  // BASE_URL: 'http://192.168.2.64:3000',
+
+  //Local
   BASE_URL: 'http://10.0.2.2:3000',
 };
